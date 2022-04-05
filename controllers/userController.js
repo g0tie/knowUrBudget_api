090f7f0e6c353e1e20afcdfc761904f5a2a9f325
g0tie <gaultierbegue@outlook.com>
2022-04-05 11:51:43 +0400

    write addExpense controller method

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,7 +16,7 @@ exports.getExpenses = async (req, res) => {
             }
         });
 
-        return res.status(200).send({message: "Epenses found", expenses});
+        return res.status(200).send({message: "Expenses found", expenses});
 
     } catch (e) {
         return res.status(500).send({message: `Error has occured: ${e}`});
@@ -26,9 +26,19 @@ exports.getExpenses = async (req, res) => {
 exports.addExpense = async (req, res) => {
     try {
         const userId = await jwt.verify(req.session.token, config.secret).id;
+        const date = new Date();
+        const expenses = Expense.create({
+            userId,
+            name: req.body.name,
+            type: req.body.type,
+            amount: req.body.amount,
+            date: date.toISOString()
+        });
+
+        if (!expenses) return res.status(400).send({message: "Error, could not add expense"});
         
     } catch (e) {
-        res.status(500).send({message: `Error has occured ${e}`});
+        return res.status(500).send({message: `Error has occured ${e}`});
     }
 }
 
@@ -36,7 +46,7 @@ exports.getLimit = async (req, res) => {
     try {
 
     } catch (e) {
-        res.status(500).send({message: `Error has occured ${e}`});
+        return res.status(500).send({message: `Error has occured ${e}`});
     }
 }
 
@@ -44,6 +54,6 @@ exports.setLimit = async (req, res) => {
     try {
 
     } catch (e) {
-        res.status(500).send({message: `Error has occured ${e}`});
+        return res.status(500).send({message: `Error has occured ${e}`});
     }
 }
