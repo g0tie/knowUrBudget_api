78aecc1cb6e3eee9eea44de385d152cf44f5e155
g0tie <gaultierbegue@outlook.com>
2022-09-22 15:03:04 +0400

    fix add retrieving of csrf token in get queries

--- middleware/csrf.js
+++ middleware/csrf.js
@@ -1,9 +1,10 @@
 var { randomBytes } = require('crypto');
 
 checkCSRF = (req, res, next) => {
-	if (req.body.csrf !== req.session.csrf) return res.status(403).json({message:"Unauthorized !"});
-    req.session.csrf = randomBytes(100).toString('base64'); // convert random data to a string
-
+    const clientCsrfToken = req.body.csrf || req.params.csrf || req.query.csrf;
+    
+	if (clientCsrfToken !== req.session.csrf) return res.status(403).json({message:"Unauthorized !"});
+    req.session.csrf = randomBytes(100).toString('base64'); 
     next();
 
 }
